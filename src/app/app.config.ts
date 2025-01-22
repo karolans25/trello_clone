61a12437a8e829cbc8c3ad3e5ffec9c8e8230bcc
Carolina Pulido <dev.carolina.pulido01@gmail.com>
2025-01-22 02:16:27 -0500

    opcional (add Title Strategy con router state)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, Injectable, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, RouterStateSnapshot, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration, Title, withEventReplay } from '@angular/platform-browser';
+
+@Injectable({ providedIn: 'root' })
+export class TemplatePageTitleStrategy extends TitleStrategy {  
+  constructor(private readonly title: Title) {    
+    super();  
+  }  
+  
+  override updateTitle(routerState: RouterStateSnapshot) {    
+    const title = this.buildTitle(routerState);    
+    if (title !== undefined) {      
+      this.title.setTitle(`My Application | ${title}`);    
+    }  
+  }
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    {
+      provide: TitleStrategy, useClass: TemplatePageTitleStrategy
+    }
+  ]
 };
